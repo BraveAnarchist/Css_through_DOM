61129658f412ed6d3b34c66b7b7309d186b787bc
Anarchist <103557203+BraveAnarchist@users.noreply.github.com>
2024-04-16 16:41:51 +0530

    Create inde.js

---
+++ inde.js
@@ -0,0 +1,8 @@
+let a=document.getElementById("div");
+a.style.backgroundColor="yellow";
+a.style.margin="20px";
+a.style.padding="10px";
+a.style.fontSize="18px";
+a.style.fontWeight="bold";
+a.style.height="200px";
+a.style.width="300px";
